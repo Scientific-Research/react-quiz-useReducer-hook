a4295ae54d56eb9d12c03fae8a6e1db5a59b86eb
Maximilian <scientificresearch309@gmail.com>
2024-05-17 22:11:08 +0200

    fetch the questions works well

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import Header from "./components/Header";
 import { Main } from "./components/main";
 
 export default function App() {
+  useEffect(() => {
+    const getQuestions = async () => {
+      const data = await fetch("http://localhost:8000/questions");
+      const res = await data.json();
+      console.log(res);
+    };
+    getQuestions();
+  }, []);
+
   return (
     <div className="App">
       {/* <DateCounter /> */}
