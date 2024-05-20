2706eb80753c6133767b83e28cf753f276cc49b8
Maximilian <scientificresearch309@gmail.com>
2024-05-20 03:11:08 +0200

    classname for button added

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -4,8 +4,8 @@ export const StartScreen = ({ questions }: any) => {
   return (
     <div className="start">
       <h2>Welcome to the React Quiz!</h2>
-      <h3>{questions.length} question to test your React mastery!</h3>
-      <button>Let's start!</button>
+      <h3>{questions.length} question(s) to test your React mastery!</h3>
+      <button className="btn btn-ui">Let's start!</button>
     </div>
   );
 };
