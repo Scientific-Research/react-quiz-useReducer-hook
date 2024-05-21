a93a9f78d720545622b39801f44238216226ae1f
Maximilian <scientificresearch309@gmail.com>
2024-05-22 01:43:46 +0200

    and now, we get the selected answer with a little bit indent

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -17,7 +17,10 @@ export const Options = ({
       {question.options.map((option: string, index) => (
         <button
           key={option}
-          className="btn btn-option"
+          // className="btn btn-option"
+          className={`btn btn-option ${
+            index === Number(answer) ? "answer" : ""
+          }`}
           onClick={() => dispatch({ type: "newAnswer", payload: index })}
         >
           {option}
