815bae3642bb28c03c9ba59f2114f1e83ac07da0
Maximilian <scientificresearch309@gmail.com>
2024-05-21 21:00:30 +0200

    displaying now the question on the browswer

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -2,5 +2,9 @@ import { IQuestion } from "../interfaces/interfaces";
 
 export const Question = ({ question }: { question: IQuestion }) => {
   console.log(question);
-  return <div>Question</div>;
+  return (
+    <>
+      <h4>{question.question}</h4>
+    </>
+  );
 };
