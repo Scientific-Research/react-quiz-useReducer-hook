1f5636c5b3e717b9c7b729821f45e8bcedd9b1b6
Maximilian <scientificresearch309@gmail.com>
2024-05-23 00:50:08 +0200

    the total point and points for every section using reduce function works well

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -9,8 +9,11 @@ export const Progress = ({
   questions: IQuestion[];
   points: number;
 }) => {
-  let maxPossiblePoints = 0;
-  maxPossiblePoints = maxPossiblePoints + points;
+  // let maxPossiblePoints = 0;
+  // maxPossiblePoints = maxPossiblePoints + points;
+  // or we can use reducer in JavaScript:
+  const maxPossiblePoints = questions.reduce((cur, acc) => cur + acc.points, 0);
+
   return (
     <>
       <header className="progress">
