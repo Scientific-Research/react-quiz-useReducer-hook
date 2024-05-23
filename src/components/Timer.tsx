c8860c91a79da0483ababe87aa2f3fe78308f7fd
Maximilian <scientificresearch309@gmail.com>
2024-05-24 01:35:26 +0200

    using clearInterval(id), we can unmount the timer after every one second and now the timer works perfect

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -8,13 +8,15 @@ export const Timer = ({
   secondRemaining: number;
 }) => {
   useEffect(() => {
-    setInterval(() => {
+    const id = setInterval(() => {
       // console.log("tick");
       dispatch({
         type: "tick",
         payload: null,
       });
     }, 1000);
+    // to clear up the timer(unmount the timer), once we start the quiz again!
+    return () => clearInterval(id);
   }, [dispatch]);
   return <div className="timer">{secondRemaining}</div>;
 };
