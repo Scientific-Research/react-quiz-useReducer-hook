392d25820160424aeade723a385d9d21d9cbfefa
Maximilian <scientificresearch309@gmail.com>
2024-05-24 00:39:18 +0200

    setInterval will tick every 1000 msec

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,5 +1,10 @@
-export const Timer = () =>{
-  return(
-    <div>Timer</div>
-  )
-}
+import { useEffect } from "react";
+
+export const Timer = () => {
+  useEffect(() => {
+    setInterval(() => {
+      console.log("tick");
+    }, 1000);
+  }, []);
+  return <div className="timer">05:00</div>;
+};
